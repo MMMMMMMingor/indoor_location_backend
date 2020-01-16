908313faa3f4758116fa15677d893cc908037506
xwm <947462457@qq.com>
2020-01-16 10:44:53 +0800

    修复阿里云导致的邮件发送错误

--- src/test/java/com/scut/indoorLocation/service/impl/MailServiceTest.java
+++ src/test/java/com/scut/indoorLocation/service/impl/MailServiceTest.java
@@ -3,7 +3,6 @@ package com.scut.indoorLocation.service.impl;
 import com.scut.indoorLocation.service.MailService;
 import com.scut.indoorLocation.utility.LevelDBUtil;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Ignore;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -29,5 +28,7 @@ class MailServiceTest {
     @Test
     void sendMail() {
         mailService.sendMail("947462457@qq.com", "邮件验证码");
+
+        log.info(levelDBUtil.get("947462457@qq.com", String.class));
     }
 }
