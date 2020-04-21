b5e0bb378c4f8c48d5c707bf3f866fdc5cbdfa85
xwm <947462457@qq.com>
2020-04-21 19:19:08 +0800

    修复单词拼写的大小写错误.

--- src/main/java/com/scut/indoorLocation/dto/FingerPrintMetaDetailResponse.java
+++ src/main/java/com/scut/indoorLocation/dto/FingerPrintMetaDetailResponse.java
@@ -18,10 +18,10 @@ public class FingerPrintMetaDetailResponse {
     @ApiModelProperty(value = "元数据ID", name = "metadataId", example = "36ed8eba21765f8595fe8d45b7892ba6")
     private String metadataId;
 
-    @ApiModelProperty(value = "ap信息", name = "accessPoints", example = "null")
+    @ApiModelProperty(value = "ap信息", name = "accessPoints", example = "{\"bssid\":\"78:44:fd:61:ce:d6\",\"ssid\":\"3-303_5\",\"x\":25.0,\"y\":20.0}")
     private List<AccessPoint> accessPoints;
 
-    @ApiModelProperty(value = "指纹数", name = "count", example = "10")
+    @ApiModelProperty(value = "指纹数", name = "fingerPrints", example = "{\"x\":25.0,\"y\":30.0,\"intensities\":[0,-47,-52],\"createTime\":\"2020-03-13T18:59:42.756\",\"aps\":[0,-47,-52],\"positionX\":25.0,\"positionY\":30.0}")
     private List<FingerPrint2D> fingerPrints;
 
 }
